da35c8f54e2c36e87ca61cdcc7acb7f2f9c6b105
avaloshka <81110315+avaloshka@users.noreply.github.com>
2021-06-26 17:39:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime
+import smtplib
+
+import pandas
+import random
+
+EMAIL = "business.alexvaloshka@gmail.com"
+PASSWORD = "T|(LyXh5bN(k<*q*"
+
+today = datetime.datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday!\n\n{contents}"
+                            )
